f191778e1de795e0cb48fe8a8d8875ab06891cbc
Anuj <anujsh1989@gmail.com>
2022-01-17 21:51:17 +0530

    saved

---
+++ test.js
@@ -0,0 +1,37 @@
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log("val if i is", i);
+//   for (let j = i + 1; j < arr.length; j++) {
+//     console.log("val of j is", j);
+//     console.log("val of arr i and j is", arr[i], arr[j]);
+//     if (arr[j] > arr[i]) {
+//       let temp = arr[j];
+//       arr[j] = arr[i];
+//       arr[i] = temp;
+//     }
+//     console.log("arr is", arr);
+//   }
+// }
+// console.log(arr);
+
+// function swap(a, b) {
+//   let temp = b;
+//   b = a;
+//   a = temp;
+//   console.log("test", a, b);
+// }
+// swap(5, 10);
+
+// function isPrime(num) {
+//   for (let i = 2; i <= num; i++) {
+//     if (num % i === 0) {–––
+//       console.log("Not Prime");
+//       return;
+//     } else {
+//       console.log("prime");
+//       return;
+//     }
+//   }
+// }
+// isPrime(4);
